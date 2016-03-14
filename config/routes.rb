b38849c578ca8c34e012bd7c6a3dec381157eb6a
Aaron Mackall <aaronmckll@gmail.com>
2016-03-13 21:16:14 -0400

    Checkpoint 26 testing: completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+   get 'welcome/contact'
+
+  get 'welcome/faq'
+
+  root to: 'welcome#index'
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +65,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end

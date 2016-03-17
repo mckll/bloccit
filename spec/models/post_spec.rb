3b390d5b3ed176a92e56a1d24b0d6a285b97dafa
Aaron Mackall <aaronmckll@gmail.com>
2016-03-16 23:38:54 -0400

    Checkpoint 28 Models: completed

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:post) { Post.create!(title: "New Post Title", body: "New Post Body") }
+
+  describe "attributes" do
+# #2
+    it "responds to title" do
+      expect(post).to respond_to(:title)
+    end
+# #3
+    it "responds to body" do
+      expect(post).to respond_to(:body)
+    end
+  end
+end

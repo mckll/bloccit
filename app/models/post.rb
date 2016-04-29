509caaf0c18a3db23988debe54a91d3bcbd4df79
Aaron Mackall <aaronmckll@gmail.com>
2016-04-29 18:50:24 -0400

    Assignment 44 completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,9 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
-
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
+  after_create :create_favorite
 
   default_scope { order('rank DESC') }
 
@@ -35,5 +35,10 @@ class Post < ActiveRecord::Base
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  def create_favorite
+    Favorite.create(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deliver_now
   end
 end

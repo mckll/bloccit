72aebb41a0b96b410dcd831a4034c531f5026fc8
Aaron Mackall <aaronmckll@gmail.com>
2016-05-09 22:19:59 -0400

    Checkpoint 48 API: completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   before_save { self.role ||= :member }
 
+  before_create :generate_auth_token
+
 # #3
  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 # #4
@@ -31,5 +33,12 @@ validates :password, length: { minimum: 6 }, allow_blank: true
    def avatar_url(size)
      gravatar_id = Digest::MD5::hexdigest(self.email).downcase
      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+   end
+
+   def generate_auth_token
+     loop do
+       self.auth_token = SecureRandom.base64(64)
+       break unless User.find_by(auth_token: auth_token)
+     end
    end
 end
